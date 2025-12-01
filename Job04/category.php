967267f118cc81092f38d7e818c1f1ec6c881002
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-12-01 15:04:19 +0100

    création du fichier index.php qui va permettre de récuperer un produit de id 7 qui est une paire d'enceintes design

---
+++ Job04/category.php
@@ -0,0 +1,73 @@
+<?php
+
+class Category {
+   
+    private ?int $id;
+    private ?string $name;
+    private ?string $description;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+
+    /**
+     * Constructeur de la classe Category.
+     */
+    public function __construct(
+        ?int $id = null,
+        ?string $name = null,
+        ?string $description = null,
+        DateTime $createdAt = new DateTime(),
+        DateTime $updatedAt = new DateTime()
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    }
+
+
+    // les getters
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getDescription(): string {
+        return $this->description;
+    }
+
+    public function getCreatedAt(): DateTime {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime {
+        return $this->updatedAt;
+    }
+
+    // les setters
+
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+
+    public function setDescription(string $description): void {
+        $this->description = $description;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void {
+        $this->createdAt = $createdAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void {
+        $this->updatedAt = $updatedAt;
+    }
+}
+?>
---
+++ Job04/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'product.php';
+require_once 'category.php';
+
+$host = 'localhost';
+$db   = 'draft-shop'; 
+$user = 'root';             
+$pass = '';            
+$charset = 'utf8mb4';
+
+
+
+?>
